fdf0b89303572152c8f6f3b452708a19be13385a
Nelson Loyola <loyola@cs.ualberta.ca>
2009-07-08 10:43:16 -0600

    added constants that represent DLL return codes

--- src/edu/ualberta/med/scanlib/ScanLib.java
+++ src/edu/ualberta/med/scanlib/ScanLib.java
@@ -2,6 +2,25 @@
 package edu.ualberta.med.scanlib;
 
 public abstract class ScanLib {
+    // Error codes returned by methods
+    public static final int SC_SUCCESS = 0;
+    public static final int SC_FAIL = -1;
+    public static final int SC_TWAIN_UAVAIL = -2;
+    public static final int SC_CALIBRATOR_NO_REGIONS = -3;
+    public static final int SC_CALIBRATOR_ERROR = -4;
+    public static final int SC_INI_FILE_ERROR = -5;
+    public static final int SC_INVALID_DPI = -6;
+    public static final int SC_INVALID_PLATE_NUM = -7;
+    public static final int SC_FILE_SAVE_ERROR = -8;
+    public static final int SC_INVALID_VALUE = -9;
+    public static final int SC_INVALID_IMAGE = -10;
+
+    public static final int DPI_300 = 300;
+
+    public static final int DPI_400 = 400;
+
+    public static final int DPI_600 = 600;
+    
     public abstract int slIsTwainAvailable();
 
     public abstract int slSelectSourceAsDefault();
