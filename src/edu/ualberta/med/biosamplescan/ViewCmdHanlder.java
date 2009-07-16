5a7e95153cab3792134e6be36d0aaec654e049d2
Thomas Polasek <tpolasek@cs.ualberta.ca>
2009-07-16 12:45:52 -0600

    Development yo.

---
+++ src/edu/ualberta/med/biosamplescan/ViewCmdHanlder.java
@@ -0,0 +1,21 @@
+package edu.ualberta.med.biosamplescan;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ViewCmdHanlder extends AbstractHandler implements IHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbenchWindow window = HandlerUtil
+				.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(window.getShell(),
+				"MenuEclipseArticle Plug-in", "Hello, Eclipse world");
+		return null;
+	}
+
+}
