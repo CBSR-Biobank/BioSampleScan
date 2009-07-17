741489a81660bc07e31505ef812e8700fac20961
Nelson Loyola <loyola@cs.ualberta.ca>
2009-07-17 16:24:47 -0600

    app now closes if it cant find TWAIN driver

--- src/edu/ualberta/med/biosamplescan/View.java
+++ src/edu/ualberta/med/biosamplescan/View.java
@@ -2,36 +2,61 @@ package edu.ualberta.med.biosamplescan;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 
 import edu.ualberta.med.biosamplescan.gui.Main;
+import edu.ualberta.med.scanlib.ScanLib;
+import edu.ualberta.med.scanlib.ScanLibFactory;
 
 public class View extends ViewPart {
-	public static final String ID = "edu.ualberta.med.biosamplescan.view";
+    public static final String ID = "edu.ualberta.med.biosamplescan.view";
 
-	private Main main;
+    private Main main = null;
 
-	public View() {
-		BioSampleScanPlugin.getDefault().addView(this);
-	}
+    public View() {
+        BioSampleScanPlugin.getDefault().addView(this);
+    }
 
-	@Override
-	public void dispose() {
-		BioSampleScanPlugin.getDefault().removeView(this);
+    @Override
+    public void dispose() {
+        BioSampleScanPlugin.getDefault().removeView(this);
 
-	}
+    }
 
-	public void createPartControl(Composite parent) {
-		main = new Main(parent, SWT.BORDER);
-	}
+    public void createPartControl(Composite parent) {
+        // TODO: MOVE FOLLOWING CODE TO TREE VIEWER VIEW WHEN READY
+        //
+        String osname = System.getProperty("os.name");
+        if (osname.startsWith("Windows")) {
+            ScanLib scanLib = ScanLibFactory.getScanLib();
+            if (scanLib.slIsTwainAvailable() != ScanLib.SC_SUCCESS) {
+                Display.getDefault().asyncExec(new Runnable() {
+                    public void run() {
+                        BioSampleScanPlugin.openError("TWAIN Driver Error",
+                            "TWAIN driver not installed on this computer.");
+                        PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                            .close();
+                    }
+                });
+                return;
+            }
+        }
+        // MOVE ABOVE CODE TO TREE VIEWER VIEW WHEN READY
 
-	public Main getMain() {
-		return main;
-	}
+        main = new Main(parent, SWT.BORDER);
+    }
 
-	@Override
-	public void setFocus() {
-		main.setFocus();
-	}
+    public Main getMain() {
+        return main;
+    }
+
+    @Override
+    public void setFocus() {
+        if (main == null)
+            return;
+        main.setFocus();
+    }
 
 }
