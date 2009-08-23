d1acea6aae1276cfdb82baed4579b2b243a9423b
Nelson Loyola <nloyola@gmail.com>
2009-08-22 18:11:37 -0600

    working on moving configuration to preferences

--- src/edu/ualberta/med/biosamplescan/BioSampleScanPlugin.java
+++ src/edu/ualberta/med/biosamplescan/BioSampleScanPlugin.java
@@ -1,5 +1,8 @@
-
 package edu.ualberta.med.biosamplescan;
+
+import jargs.gnu.CmdLineParser;
+import jargs.gnu.CmdLineParser.Option;
+import jargs.gnu.CmdLineParser.OptionException;
 
 import java.net.URL;
 
@@ -8,6 +11,7 @@ import org.eclipse.core.commands.State;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
@@ -50,9 +54,6 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
     public void start(BundleContext context) throws Exception {
         super.start(context);
         plugin = this;
-
-        palletSet = new PalletSet();
-        ConfigSettings.getInstance();
     }
 
     @Override
@@ -69,8 +70,8 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
                 ImageDescriptor desc = ImageDescriptor.createFromURL(url);
                 registry.put(key, desc);
             }
+        } catch (Exception e) {
         }
-        catch (Exception e) {}
     }
 
     /*
@@ -99,8 +100,7 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
      * Returns an image descriptor for the image file at the given plug-in
      * relative path
      * 
-     * @param path
-     *            the path
+     * @param path the path
      * @return the image descriptor
      */
     public static ImageDescriptor getImageDescriptor(String path) {
@@ -111,27 +111,24 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
      * Display an information message
      */
     public static void openMessage(String title, String message) {
-        MessageDialog.openInformation(
-            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-            title, message);
+        MessageDialog.openInformation(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), title, message);
     }
 
     /**
      * Display an error message
      */
     public static void openError(String title, String message) {
-        MessageDialog.openError(
-            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-            title, message);
+        MessageDialog.openError(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), title, message);
     }
 
     /**
      * Display an information message
      */
     public static boolean openConfirm(String title, String message) {
-        return MessageDialog.openConfirm(
-            PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-            title, message);
+        return MessageDialog.openConfirm(PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow().getShell(), title, message);
     }
 
     /**
@@ -140,9 +137,8 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
     public static void openAsyncError(final String title, final String message) {
         Display.getDefault().asyncExec(new Runnable() {
             public void run() {
-                MessageDialog.openError(
-                    PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
-                    title, message);
+                MessageDialog.openError(PlatformUI.getWorkbench()
+                    .getActiveWorkbenchWindow().getShell(), title, message);
             }
         });
     }
@@ -165,18 +161,31 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
     }
 
     public void setPlateSetView(PalletSetView plateSetEditor) {
+        final String err = parseCommandLine();
+        if (err != null) {
+            stopApplication("Command Line Arguments", err);
+            return;
+        }
+
+        palletSet = new PalletSet();
+        ConfigSettings.getInstance();
         plateSetView = plateSetEditor;
 
-        IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-        ISourceProviderService service = (ISourceProviderService) window.getService(ISourceProviderService.class);
+        IWorkbenchWindow window = PlatformUI.getWorkbench()
+            .getActiveWorkbenchWindow();
+        ISourceProviderService service = (ISourceProviderService) window
+            .getService(ISourceProviderService.class);
 
-        DebugState debugStateSourceProvider = (DebugState) service.getSourceProvider(DebugState.SESSION_STATE);
-        debugStateSourceProvider.setState(BioSampleScanPlugin.getDefault().isDebugging());
+        DebugState debugStateSourceProvider = (DebugState) service
+            .getSourceProvider(DebugState.SESSION_STATE);
+        debugStateSourceProvider.setState(BioSampleScanPlugin.getDefault()
+            .isDebugging());
 
         // reads the persisted state for the menu contribution
-        ICommandService cmdService = (ICommandService) PlatformUI.getWorkbench().getService(
-            ICommandService.class);
-        Command command = cmdService.getCommand("edu.ualberta.med.biosamplescan.menu.debug.simulateScanning");
+        ICommandService cmdService = (ICommandService) PlatformUI
+            .getWorkbench().getService(ICommandService.class);
+        Command command = cmdService
+            .getCommand("edu.ualberta.med.biosamplescan.menu.debug.simulateScanning");
         State state = command.getState("org.eclipse.ui.commands.toggleState");
         ConfigSettings.getInstance().setSimulateScanning(
             (Boolean) state.getValue());
@@ -186,12 +195,56 @@ public class BioSampleScanPlugin extends AbstractUIPlugin {
                 String msg = new String();
                 if (ConfigSettings.getInstance().getPalletCount() == 0) {
                     msg = "Please configure scanner.";
-                }
-                else {
+                } else {
                     msg = "Configuration loaded.";
                 }
                 plateSetView.updateStatusBar(msg);
                 plateSetView.refresh();
+            }
+        });
+    }
+
+    private String parseCommandLine() {
+        CmdLineParser parser = new CmdLineParser();
+        Option outputOpt = parser.addStringOption('o', "output");
+        Option palletsMaxOpt = parser.addIntegerOption('p', "palletsmax");
+
+        try {
+            parser.parse(Platform.getApplicationArgs());
+        } catch (OptionException e) {
+            return e.getMessage();
+        }
+
+        ConfigSettings c = ConfigSettings.getInstance();
+
+        String filename = (String) parser.getOptionValue(outputOpt);
+        if (filename != null) {
+            if (filename.length() == 0) {
+                return "Invalid save location";
+            }
+            c.setSaveFileName(filename);
+        }
+
+        Integer palletsMax = (Integer) parser.getOptionValue(palletsMaxOpt);
+        if (palletsMax != null) {
+            if ((palletsMax <= 0) || (palletsMax > ConfigSettings.PALLET_NUM)) {
+                return "nvalid value. palletsmax should be between 1 and "
+                    + ConfigSettings.PALLET_NUM;
+            }
+            c.setPalletsMax(palletsMax);
+        }
+
+        if ((filename != null) && (palletsMax != null) && (palletsMax != 1)) {
+            return "palletsmax should be 1 when using --output option";
+        }
+        return null;
+    }
+
+    public static void stopApplication(final String title, final String msg) {
+        Display.getDefault().asyncExec(new Runnable() {
+            public void run() {
+                BioSampleScanPlugin.openError(title, msg);
+                PlatformUI.getWorkbench().getActiveWorkbenchWindow().close();
             }
         });
     }
--- src/edu/ualberta/med/biosamplescan/Startup.java
+++ src/edu/ualberta/med/biosamplescan/Startup.java
@@ -1,12 +1,6 @@
 
 package edu.ualberta.med.biosamplescan;
 
-import jargs.gnu.CmdLineParser;
-import jargs.gnu.CmdLineParser.Option;
-import jargs.gnu.CmdLineParser.OptionException;
-
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -21,12 +15,6 @@ public class Startup implements IStartup {
         final IWorkbench workbench = PlatformUI.getWorkbench();
         workbench.getDisplay().asyncExec(new Runnable() {
             public void run() {
-                final String err = parseCommandLine();
-                if (err != null) {
-                    stopApplication("Command Line Arguments", err);
-                    return;
-                }
-
                 if (ConfigSettings.getInstance().getSimulateScanning()) return;
 
                 String osname = System.getProperty("os.name");
@@ -34,57 +22,11 @@ public class Startup implements IStartup {
                 if (osname.startsWith("Windows")) {
                     final int result = ScanLib.getInstance().slIsTwainAvailable();
                     if (result != ScanLib.SC_SUCCESS) {
-                        stopApplication("Driver Error",
+                        BioSampleScanPlugin.stopApplication("Driver Error",
                             ScanLib.getErrMsg(result));
                         return;
                     }
                 }
-            }
-        });
-    }
-
-    private String parseCommandLine() {
-        CmdLineParser parser = new CmdLineParser();
-        Option outputOpt = parser.addStringOption('o', "output");
-        Option palletsMaxOpt = parser.addIntegerOption('p', "palletsmax");
-
-        try {
-            parser.parse(Platform.getApplicationArgs());
-        }
-        catch (OptionException e) {
-            return e.getMessage();
-        }
-
-        ConfigSettings c = ConfigSettings.getInstance();
-
-        String filename = (String) parser.getOptionValue(outputOpt);
-        if (filename != null) {
-            if (filename.length() == 0) {
-                return "Invalid save location";
-            }
-            c.setSaveFileName(filename);
-        }
-
-        Integer palletsMax = (Integer) parser.getOptionValue(palletsMaxOpt);
-        if (palletsMax != null) {
-            if ((palletsMax <= 0) || (palletsMax > ConfigSettings.PALLET_NUM)) {
-                return "nvalid value. palletsmax should be between 1 and "
-                    + ConfigSettings.PALLET_NUM;
-            }
-            c.setPalletsMax(palletsMax);
-        }
-
-        if ((filename != null) && (palletsMax != null) && (palletsMax != 1)) {
-            return "palletsmax should be 1 when using --output option";
-        }
-        return null;
-    }
-
-    private void stopApplication(final String title, final String msg) {
-        Display.getDefault().asyncExec(new Runnable() {
-            public void run() {
-                BioSampleScanPlugin.openError(title, msg);
-                PlatformUI.getWorkbench().getActiveWorkbenchWindow().close();
             }
         });
     }
